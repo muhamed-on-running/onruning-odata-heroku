df0d703b4e5003c651ed446b2da09c1a94d74131
mbajramovic <mbajramovic@echots.com>
2020-11-10 23:32:14 +0100

    dummy filter

--- src/index.js
+++ src/index.js
@@ -4,103 +4,112 @@ const fs = require('fs')
 const port = process.env.PORT || 5005;
 const bodyParser = require('body-parser');
 const app = express();
+const querystring = require('querystring');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname+'/pages/index.html'))
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname + '/pages/index.html'))
 );
 
 //Sales order header and Sales order line 
-app.get('/odata/sohs/\\$metadata', (req, res) => 
+app.get('/odata/sohs/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/soh-only.xml'))
 );
 
-app.get('/odata/sohs-hsos/\\$metadata', (req, res) => 
+app.get('/odata/sohs-hsos/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/soh-hso.xml'))
 );
 
 //HSOs with partner attribute in NavigationProperty
-app.get('/odata/hsos/partner/\\$metadata', (req, res) => 
+app.get('/odata/hsos/partner/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/hsos-partner.xml'))
 );
 
 //HSOs without partner attribute in NavigationProperty
-app.get('/odata/hsos/nopartner/\\$metadata', (req, res) => 
+app.get('/odata/hsos/nopartner/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/hsos-only.xml'))
 );
 
 //Single HSO with no relations
-app.get('/odata/hso/\\$metadata', (req, res) => 
+app.get('/odata/hso/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/hso-single.xml'))
 );
 
-app.get('/external/metadata.svc/', (req, res) => 
+app.get('/external/metadata.svc/', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/external-data-service.xml'))
 );
 
-app.get('/external/metadata.svc/\\$metadata', (req, res) => 
+app.get('/external/metadata.svc/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'xml/metadata/external-metadata-example.xml'))
 );
 
-app.get('/external/metadata.svc/\\$metadata#Categorys', (req, res) => 
-    { 
-        fs.readFile(path.join(__dirname, 'xml/metadata/external-data-example.json'), 'utf8', (err, data) => {
-            if (err) {
+app.get('/external/metadata.svc/\\$metadata#Categorys', (req, res) => {
+    fs.readFile(path.join(__dirname, 'xml/metadata/external-data-example.json'), 'utf8', (err, data) => {
+        if (err) {
             throw err;
-            }
+        }
 
-            res.send(JSON.parse(data));
-        })
-    }
+        res.send(JSON.parse(data));
+    })
+}
 );
 
-app.get('/external/metadata.svc/Categorys', (req, res) => 
-    { 
-        fs.readFile(path.join(__dirname, 'xml/metadata/external-data-example.json'), 'utf8', (err, data) => {
-            if (err) {
+app.get('/external/metadata.svc/Categorys', (req, res) => {
+    fs.readFile(path.join(__dirname, 'xml/metadata/external-data-example.json'), 'utf8', (err, data) => {
+        if (err) {
             throw err;
-            }
-            res.setHeader("Preference-Applied", "odata.track-changes")
-            res.send(JSON.parse(data));
-        })
-    }
+        }
+        res.setHeader("Preference-Applied", "odata.track-changes")
+        res.send(JSON.parse(data));
+    })
+}
 );
 
-app.get('/json/data', (req, res) => 
-    { 
-        console.log(path.join(__dirname, 'json/example.json'));
-        fs.readFile(path.join(__dirname, 'json/example.json'), 'utf8', (err, data) => {
-            if (err) {
+app.get('/json/data', (req, res) => {
+    console.log(path.join(__dirname, 'json/example.json'));
+    fs.readFile(path.join(__dirname, 'json/example.json'), 'utf8', (err, data) => {
+        if (err) {
             throw err;
-            }
+        }
 
-            res.send(JSON.parse(data));
-        })
-    }
+        res.send(JSON.parse(data));
+    })
+}
 );
 
 /*****************/
 /* new endpoints */
 https://odata-metadata-test.herokuapp.com/example/service/
-app.get('/example/service/', (req, res) => 
+app.get('/example/service/', (req, res) =>
     res.sendFile(path.join(__dirname, 'odata/service.json'))
 );
-app.get('/example/service/\\$metadata', (req, res) => 
+app.get('/example/service/\\$metadata', (req, res) =>
     res.sendFile(path.join(__dirname, 'odata/metadata.xml'))
 );
 //example/service
-app.get('/example/service/Dummies', (req, res) => 
-    res.sendFile(path.join(__dirname, 'odata/setList.json'))
-);
+app.get('/example/service/Dummies', (req, res) => {
+    var url = require('url');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query.$filter;
+    if (query.includes('1')) {
+        res.sendFile(path.join(__dirname, 'odata/firstItem.json'));
+    }
+    else if (query.includes('2')) {
+        res.sendFile(path.join(__dirname, 'odata/secondItem.json'))
+    }
+    else {
+        res.sendFile(path.join(__dirname, 'odata/setList.json'))
+    }
+});
 
-app.get('/example/service/Dummies[(]1[)]', (req, res) => 
+app.get('/example/service/Dummies[(]1[)]', (req, res) =>
     res.sendFile(path.join(__dirname, 'odata/firstItem.json'))
 );
 
-app.get('/example/service/Dummies[(]2[)]', (req, res) => 
+app.get('/example/service/Dummies[(]2[)]', (req, res) =>
     res.sendFile(path.join(__dirname, 'odata/secondItem.json'))
 );
 
